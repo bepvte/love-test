5344efdb9fab5a720149fb7b2bebee49fe5c776b
bep <8226605+bepvte@users.noreply.github.com>
2017-10-19 16:48:57 -0400

    rad

--- controls.lua
+++ controls.lua
@@ -5,5 +5,6 @@ return {
 	['a'] = function(c, dt) c.acc = c.acc + vector(-1 * c.speed * dt, 0) end,
 	['s'] = function(c, dt) c.acc = c.acc + vector(0, 1 * c.speed * dt) end,
 	['d'] = function(c, dt) c.acc = c.acc + vector(1 * c.speed * dt, 0) end,
-	['q'] = function() love.event.quit() end
+	['q'] = function() love.event.quit() end,
+	['r'] = function() require("lurker/lurker").scan() end
 }
